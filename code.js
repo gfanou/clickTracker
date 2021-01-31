5f18759178320bc97d6da708e1f196c64b060c73
gfanou <galleryneil@gmail.com>
2021-01-31 15:56:33 -0600

    Initial committ

---
+++ code.js
@@ -0,0 +1,11 @@
+let clicks = 0;
+
+function addClick() {
+    clicks = clicks + 1;
+
+}
+
+// Simulate click function
+function button2() {
+    alert("The first button was clicked " + (clicks) + " times");
+}
